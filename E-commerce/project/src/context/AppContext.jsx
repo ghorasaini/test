905676628170e4b6f568e671cc6f719f7e2ee905
Mingtindu <mingmartu@gmail.com>
2025-09-04 12:19:46 +0545

    fix

--- E-commerce/project/src/context/AppContext.jsx
+++ E-commerce/project/src/context/AppContext.jsx
@@ -1,29 +1,25 @@
-import { Children, createContext, useState } from "react";
+import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+export const AppContext = createContext(null);
 
+const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(null);
+  const [showUserLogin, setShowUserLogin] = useState(false);
 
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
 
-
-export  const AppContext = createContext(null);
-const AppContextProvider = ({Children})=> {
-    const navigate = useNavigate();
-    const [user, setUser]= useState(null);
-    const [isSeller, setIsSeller]= useState(null);
-    const [showUserLogin, setShowUserLogin]= useState(false);
-
-    const value = {
-         navigate,
-         user,
-          setUser,
-           setIsSeller,
-            showUserLogin,
-             setShowUserLogin
-            };
-    return
-        <AppContext.Provider value ={value}> {Children} </AppContext.Provider>;
-    
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
-
 
 export default AppContextProvider;
--- E-commerce/project/src/main.jsx
+++ E-commerce/project/src/main.jsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
-import  AppContextProvider  from "./context/AppContext.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
 
-createRoot(document.getElementById("root")).render( 
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AppContextProvider>
-       <App />
-    </AppContextProvider>
-
-   
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
   </StrictMode>
 );
